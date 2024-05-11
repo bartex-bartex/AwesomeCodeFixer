d5dca371c0c3f7e1ce741ce98da3772b78f41b2d
Gabi363 <g.zmuda@novitas.com.pl>
2024-05-11 20:43:48 +0200

    Connected Divide_Input and Linter

--- src/Modules/AwesomeCodeFixer/Divide_Input.cs
+++ src/Modules/AwesomeCodeFixer/Divide_Input.cs
@@ -33,43 +33,62 @@ namespace AwesomeCodeFixer
             var beginings1 = from line in lines.Select((Value, Index) => new { Value, Index })
                              where Regex.Match(line.Value, "^<(.*) ", RegexOptions.Singleline).Success
                              select new List<string> {line.Index.ToString(),
-                                                line.Value.Substring(line.Value.IndexOf("<")+1,
-                                                                    line.Value.IndexOf(" ")-1)};
+                                                    line.Value.Substring(line.Value.IndexOf("<")+1,
+                                                    line.Value.IndexOf(" ")-1)};
 
 
             var beginings2 = from line in lines.Select((Value, Index) => new { Value, Index })
                              where Regex.Match(line.Value, "^```(.+)", RegexOptions.Singleline).Success
-                             select new List<string> { line.Index.ToString(), "code" };
+                             select new List<string> { line.Index.ToString(), line.Value.Split("\n")[0].Remove(0,3) };
 
 
             var beginings3 = from line in lines.Select((Value, Index) => new { Value, Index })
                              where Regex.Match(line.Value, @"^(([^\$]+\$ )|(\$ ))", RegexOptions.Singleline).Success
-                             select new List<string> { line.Index.ToString(), "latex_inline", line.Index.ToString() };
+                             select new List<string> { line.Index.ToString(), "InlineLatex", line.Index.ToString() };
 
 
             var beginings4 = from line in lines.Select((Value, Index) => new { Value, Index })
                              where Regex.Match(line.Value, @"\$\$", RegexOptions.Singleline).Success
-                             select new List<string> { line.Index.ToString(), "latex_block" };
+                             select new List<string> { line.Index.ToString(), "BlockLatex" };
 
             var beginings41 = beginings4.ToList();
             for (int j = 1; j < beginings4.Count(); j += 2) beginings41.RemoveAt(j);
 
 
             var components = beginings1.Concat(beginings2).Concat(beginings3).Concat(beginings41).ToList();
-            components.Sort((x, y) => x[0].CompareTo(y[0]));
-
 
             foreach (var com in components)
             {
                 com.Add("");
                 int i = Int32.Parse(com[0]);
                 string name = com[1];
-                if (name == "code") find_end(lines, com, i + 1, "^```");
-                else if (name == "latex_block") find_end(lines, com, i + 1, @"^\$\$(.*)");
+                if (name == "BlockLatex") find_end(lines, com, i + 1, @"^\$\$(.*)");
                 else if (name == "Header" || name == "YouTube") find_end(lines, com, i, @"/>");
                 else if (name == "Note" || name == "Warning" || name == "Info" || name == "DeepDive")
                     find_end(lines, com, i, string.Format(@"</{0}>", name));
+                else if(name != "InlineLatex") find_end(lines, com, i + 1, "^```");
             }
+
+            SortedSet<int> lines_found = new SortedSet<int>(); 
+            foreach(var com in components){
+                for(int i = Int32.Parse(com[0]); i <= Int32.Parse(com[2]); i++){
+                    lines_found.Add(i);
+                }
+
+            }
+            for(int i=0; i < lines.Count(); i++) {
+                if(!lines_found.Contains(i)){
+                    int beginning = i;
+                    int end = i;
+                    while(!lines_found.Contains(i) && i+1 < lines.Count()){
+                        end = i;
+                        i++;
+                    }
+                    components.Add(new List<string>{beginning.ToString(), "Markdown", end.ToString()});
+                }
+            }
+
+            components.Sort((x, y) => Int32.Parse(x[0]).CompareTo(Int32.Parse(y[0])));
             return components;
         }
 
@@ -79,6 +98,7 @@ namespace AwesomeCodeFixer
         /// <param name="path"></param>
         /// <param name="info"></param>
         /// <returns></returns>
+        /// 
         public static List<List<string>> return_components(string path, List<List<string>> info)
         {
             List<string> lines = File.ReadAllLines(path).ToList();
@@ -98,6 +118,35 @@ namespace AwesomeCodeFixer
                 components[i].Add(content);
             }
             return components;
+        }
+        public static void get_feedback(string path, List<List<string>> info)
+        {
+            List<string> lines = File.ReadAllLines(path).ToList();
+
+            for (int i = 0; i < info.Count; i++)
+            {
+                ComponentType component;
+                if(Enum.IsDefined(typeof(ComponentType), info[i][1])){
+                    Enum.TryParse(info[i][1], out component);
+                }
+                else component = ComponentType.Markdown;
+
+                string content = "";
+                for (int j = Int32.Parse(info[i][0]); j <= Int32.Parse(info[i][2]); j++)
+                {
+                    content += lines[j];
+                    content += "\n";
+                }
+                content = content.Remove(content.Length - 1);
+
+                Console.WriteLine("-------------------------------------------");
+                Console.WriteLine("Component type: " + info[i][1]);
+                Console.WriteLine("Beginning at line: " + info[i][0]);
+                Console.WriteLine("Message:");
+                // if(component != ComponentType.InlineLatex && component != ComponentType.BlockLatex) 
+                Console.WriteLine(Linter.Lint(content, component));
+                Console.WriteLine("-------------------------------------------");
+            }
         }
     }
 }
--- src/Modules/AwesomeCodeFixer/Program.cs
+++ src/Modules/AwesomeCodeFixer/Program.cs
@@ -6,27 +6,29 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        string path = @"example_input.txt";
+        // string path = @"example_input.txt";
+        string path = @"C:..\..\test_files\example_input.txt";
 
         // tu jest lista, której każdy element jest listą i składa się z:
         // [0] - numer linii w pliku, w którym zaczyna się dany komponent
         // [1] - co to za komponent
         // [2] - numer linii, w której komponent się kończy
         List<List<string>> components_info = Divide_Input.divide(path);
-        foreach (var y in components_info)
-        {
-            Console.WriteLine(y[0] + " " + y[1] + " " + y[2]);
-        }
+        // foreach (var y in components_info)
+        // {
+        //     Console.WriteLine(y[0] + " " + y[1] + " " + y[2]);
+        // }
 
         // a tu dodatkowo lista:
         // [0] - w której linijce się zaczyna
         // [1] - cały tekst komponentu
         List<List<string>> components = Divide_Input.return_components(path, components_info);
-        foreach (var x in components)
-        {
-            Console.WriteLine(x[0]);
-            Console.WriteLine(x[1]);
-            Console.WriteLine("xxxxxxxxxx");
-        }
+        // foreach (var x in components)
+        // {
+        //     Console.WriteLine(x[0]);
+        //     Console.WriteLine(x[1]);
+        //     Console.WriteLine("xxxxxxxxxx");
+        // }
+        Divide_Input.get_feedback(path, components_info);
     }
 }
